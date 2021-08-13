e56e600729a254f84bcbbfc4a87501fc9940bdec
adam1025 <69100634+adamShiau@users.noreply.github.com>
2021-08-13 21:49:21 +0800

    update ip

---
+++ intel_IP/2018.1/GyroVarSet.v
@@ -0,0 +1,209 @@
+`timescale 1ns / 1ps
+
+module GyroVarSet (
+// avalon bus:
+    address,
+    chipselect,
+    clk,
+    rst_n,
+    write_n,
+    writedata,
+    readdata, 
+
+// var outputs:
+	o_reg0,
+    o_reg1,
+    o_reg2,
+    o_reg3,
+    o_reg4,
+    o_reg5,
+    o_reg6,
+    o_reg7,
+    o_reg8,
+    o_reg9,
+    o_reg10,
+    o_reg11,
+    o_reg12,
+    o_reg13,
+    o_reg14,
+    o_reg15,
+    o_reg16,
+    o_reg17,
+    o_reg18,
+    o_reg19,
+    o_reg20,
+    o_reg21,
+    o_reg22,
+    o_reg23,
+    o_reg24,    
+// var inputs:
+	i_var0,
+    i_var1,
+    i_var2,
+    i_var3,
+    i_var4,
+    i_var5,
+    i_var6,
+    i_var7,
+    i_var8,
+    i_var9,
+    i_var10,
+    i_var11,
+    i_var12,
+    i_var13,
+    i_var14,
+    i_var15,
+    i_var16,
+    i_var17,
+    i_var18,
+    i_var19,
+    i_var20,
+    i_var21,
+    i_var22,
+    i_var23,
+    i_var24    
+)
+;
+
+
+output reg  [ 31: 0] readdata;
+input   [  5: 0] address;
+input            chipselect;
+input            clk;
+input            rst_n;
+input            write_n;
+input   [ 31: 0] writedata;
+
+output reg [  31: 0] o_reg0, o_reg1, o_reg2, o_reg3, o_reg4, o_reg5, o_reg6, o_reg7, o_reg8, o_reg9, o_reg10;
+output reg [  31: 0] o_reg11, o_reg12, o_reg13, o_reg14, o_reg15, o_reg16, o_reg17, o_reg18, o_reg19, o_reg20;
+output reg [  31: 0] o_reg21, o_reg22, o_reg23, o_reg24;
+
+input [31:0] i_var0, i_var1, i_var2, i_var3, i_var4, i_var5, i_var6, i_var7, i_var8, i_var9, i_var10;
+input [31:0] i_var11, i_var12, i_var13, i_var14, i_var15, i_var16, i_var17, i_var18, i_var19, i_var20;
+input [31:0] i_var21, i_var22, i_var23, i_var24;
+
+
+
+
+always@(posedge clk or negedge rst_n) begin
+    if(!rst_n) readdata <= 32'd0;
+    else if(chipselect && write_n) begin
+        case(address)
+            0: readdata <= o_reg0;
+            1: readdata <= o_reg1;
+            2: readdata <= o_reg2;
+            3: readdata <= o_reg3;
+            4: readdata <= o_reg4;
+            5: readdata <= o_reg5;
+            6: readdata <= o_reg6;
+            7: readdata <= o_reg7;
+            8: readdata <= o_reg8;
+            9: readdata <= o_reg9;
+            10: readdata <= o_reg10;
+            11: readdata <= o_reg11;
+            12: readdata <= o_reg12;
+            13: readdata <= o_reg13;
+            14: readdata <= o_reg14;
+            15: readdata <= o_reg15;
+            16: readdata <= o_reg16;
+            17: readdata <= o_reg17;
+            18: readdata <= o_reg18;
+            19: readdata <= o_reg19;
+            20: readdata <= o_reg20;
+            21: readdata <= o_reg21;
+            22: readdata <= o_reg22;
+            23: readdata <= o_reg23;
+            24: readdata <= o_reg24;
+            25: readdata <= i_var0;
+            26: readdata <= i_var1;
+            27: readdata <= i_var2; 
+            28: readdata <= i_var3; 
+            29: readdata <= i_var4; 
+            30: readdata <= i_var5; 
+            31: readdata <= i_var6; 
+            32: readdata <= i_var7; 
+            33: readdata <= i_var8; 
+            34: readdata <= i_var9;
+            35: readdata <= i_var10;
+            36: readdata <= i_var11;
+            37: readdata <= i_var12;
+            38: readdata <= i_var13;
+            39: readdata <= i_var14;
+            40: readdata <= i_var15;
+            41: readdata <= i_var16;
+            42: readdata <= i_var17;
+            43: readdata <= i_var18;
+            44: readdata <= i_var19;
+            45: readdata <= i_var20;
+            46: readdata <= i_var21;
+            47: readdata <= i_var22;
+            48: readdata <= i_var23;
+            49: readdata <= i_var24;
+            default: readdata <= 32'd0;
+        endcase
+    end
+end
+  
+always@(posedge clk or negedge rst_n)
+begin
+	if (!rst_n) begin
+		o_reg0 <= 32'd125; //reg_mod_freq_cnt
+		o_reg1 <= 32'd125; //reg_mod_freq_cnt
+		o_reg2 <= 32'd4096; //reg_mod_H
+		o_reg3 <= $signed(-32'd4096); //reg_mod_L
+		o_reg4 <= 32'd0; //ADC_reg_H_offset
+		o_reg5 <= 32'd64; //Init_stable_cnt
+		o_reg6 <= 32'd4; //mv_mode
+		o_reg7 <= 32'd0; //adc_test_H
+		o_reg8 <= 0; //err_polarity
+		o_reg9 <= 0;//Diff_vth
+		o_reg10 <= 32'd0;//adc_test_L
+		o_reg11 <= 32'd0;//ladder_rst
+		o_reg12 <= 32'd4096;//reg_vth_1st_int
+		o_reg13 <= 32'd5;//err_shift_idx_pre
+		o_reg14 <= 32'd0; // w_th_p
+		o_reg15 <= 32'd0; // w_th_n
+		o_reg16 <= 32'd8190;//reg_vth
+		o_reg17 <= 32'd5;//err_shift_idx
+		o_reg18 <= 32'd1; //mod_off
+		o_reg19 <= 32'd1; //kal_Q
+		o_reg20 <= 32'd1; //kal_R
+		o_reg21 <= 32'd1; //output_mode
+		o_reg22 <= 0;
+		o_reg23 <= 0;
+		o_reg24 <= 0;
+  end
+  else if(chipselect && !write_n) begin
+    case(address)
+        0: o_reg0 <= writedata;
+        1: o_reg1 <= writedata;
+        2: o_reg2 <= writedata;
+        3: o_reg3 <= writedata;
+        4: o_reg4 <= writedata;
+        5: o_reg5 <= writedata;
+        6: o_reg6 <= writedata;
+        7: o_reg7 <= writedata;
+        8: o_reg8 <= writedata;
+        9: o_reg9 <= writedata;
+        10: o_reg10 <= writedata;
+        11: o_reg11 <= writedata;
+        12: o_reg12 <= writedata;
+        13: o_reg13 <= writedata;
+        14: o_reg14 <= writedata;
+        15: o_reg15 <= writedata;
+        16: o_reg16 <= writedata;
+        17: o_reg17 <= writedata;
+        18: o_reg18 <= writedata;
+        19: o_reg19 <= writedata;
+        20: o_reg20 <= writedata;
+        21: o_reg21 <= writedata;
+        22: o_reg22 <= writedata;
+        23: o_reg23 <= writedata;
+        24: o_reg24 <= writedata;
+    endcase
+    end
+end
+
+
+endmodule
+
---
+++ intel_IP/fog/kalman_filter_Intel_PL.v
@@ -0,0 +1,159 @@
+module Kalman_filter_Intel_PL
+#(parameter ADC_BIT = 14)
+(
+	input i_clk,
+	input i_rst_n,
+	input signed [ADC_BIT-1:0] i_meas,
+	input [31:0] i_kal_Q, i_kal_R,
+	output reg signed [31:0] x_out, p_out
+);
+
+localparam X_PRE_DLY_NUM = 19;
+localparam P_PRE_DLY_NUM = 18;
+`define P_INIT 100000
+`define P_OUT_INIT_CNT 19
+
+reg [31:0] r_kal_Q, r_kal_R;
+reg signed [31:0] r_meas, err, FB;
+reg signed [31:0] p_plus_R, p_shift;
+
+reg signed [31:0] x_pre[0:X_PRE_DLY_NUM-1];
+reg signed [31:0] p_pre[0:P_PRE_DLY_NUM-1];
+// wire [63:0] k_gain;
+wire signed [31:0] k, p_temp, FB_temp;
+reg signed [31:0] one_minus_k;
+reg [7:0]ii, kk, p_init_cnt;
+
+wire signed [31:0] measurement;
+
+assign measurement = (i_meas[ADC_BIT-1] == 1'b1)? {{32-ADC_BIT{1'b1}} , i_meas} : i_meas;
+// assign k = k_gain[63:32];
+
+always@(posedge i_clk or negedge i_rst_n) begin
+	if (!i_rst_n) begin
+		r_kal_Q <= 32'd5;
+		r_kal_R <= 32'd10;
+		r_meas <= 32'd0;
+	end
+	else begin
+		r_kal_Q <= i_kal_Q;
+		r_kal_R <= i_kal_R;
+		r_meas 	<= measurement;
+	end
+end
+
+always@(posedge i_clk or negedge i_rst_n) begin
+	if (!i_rst_n) begin
+		for (ii=0; ii<X_PRE_DLY_NUM; ii=ii+1)
+			x_pre[ii] <= 32'd0;
+			
+		for (kk=0; kk<P_PRE_DLY_NUM; kk=kk+1)
+			p_pre[kk] <= 32'd0;
+	end
+	else begin
+		p_pre[0] <= p_out + r_kal_Q;
+		x_pre[0] <= x_out;
+		
+		for (ii=0; ii<X_PRE_DLY_NUM-1; ii=ii+1)
+			x_pre[ii+1] <= x_pre[ii];
+			
+		for (kk=0; kk<P_PRE_DLY_NUM-1; kk=kk+1)
+			p_pre[kk+1] <= p_pre[kk];
+			
+	end
+end
+
+always@(posedge i_clk or negedge i_rst_n) begin
+	if (!i_rst_n) begin
+		// p_plus_R <= p_pre + r_kal_R;
+		// p_shift <= p_pre[0]<<<13;
+	end
+	else begin
+		p_plus_R <= p_pre[0] + r_kal_R;
+		p_shift <= p_pre[0]<<<13;
+	end
+end
+
+// divider_32 div_k_gain (
+  // .aclk(i_clk),                                   			// input wire aclk
+  // .aresetn(i_rst_n),                              			// input wire aresetn
+  // .s_axis_divisor_tvalid(1'b1),    							// input wire s_axis_divisor_tvalid
+  // .s_axis_divisor_tdata(p_plus_R),   	// input wire [31 : 0] s_axis_divisor_tdata, denominator
+  // .s_axis_dividend_tvalid(1'b1),  							// input wire s_axis_dividend_tvalid
+  // .s_axis_dividend_tdata(p_shift),	    // input wire [31 : 0] s_axis_dividend_tdata, numerator
+  // .m_axis_dout_tvalid(),         							// output wire m_axis_dout_tvalid
+  // .m_axis_dout_tdata(k_gain)            					// output wire [63 : 0] m_axis_dout_tdata
+// );
+divider_32	div_k_gain (
+	.aclr ( ~i_rst_n ),
+	.clock ( i_clk ),
+	.denom ( p_plus_R ),
+	.numer ( p_shift ),
+	.quotient (  ),
+	.remain ( k )
+	);
+always@(posedge i_clk or negedge i_rst_n) begin
+	if (!i_rst_n) begin
+		one_minus_k <= 32'd8192;
+	end
+	else begin
+		one_minus_k <= 32'd8192 - k;
+	end
+end
+
+// multiplier_32 mult_p_temp (
+  // .CLK(i_clk),    // input wire CLK
+  // .A(one_minus_k),        // input wire [31 : 0] A
+  // .B(p_pre[17]),        // input wire [31 : 0] B
+  // .SCLR(~i_rst_n),  // input wire SCLR
+  // .P(p_temp)        // output wire [63 : 0] P
+// );
+multiplier_32	mult_p_temp (
+	.aclr ( ~i_rst_n ),
+	.clock ( i_clk ),
+	.dataa ( one_minus_k ),
+	.datab ( p_pre[17] ),
+	.result ( p_temp )
+	);
+always@(posedge i_clk or negedge i_rst_n) begin
+	if (!i_rst_n) begin
+		p_init_cnt <= `P_OUT_INIT_CNT;
+		p_out <= `P_INIT;
+		x_out <= 32'd0;
+	end
+	
+	else begin
+		if(p_init_cnt!=0) p_init_cnt <= p_init_cnt - 1'b1;
+		else begin
+			p_out <= p_temp >> 13;
+			x_out <= x_pre[18] + FB;
+	end
+	end
+end
+
+always@(posedge i_clk or negedge i_rst_n) begin
+	if (!i_rst_n) begin
+		err <= 32'd0;
+		FB <= 32'd0;
+	end
+	else begin
+		err <= r_meas - x_pre[15];
+		FB <= (FB_temp >>> 13);
+	end
+end
+
+// multiplier_32 mult_FB_temp (
+  // .CLK(i_clk),    // input wire CLK
+  // .A(k),        // input wire [31 : 0] A
+  // .B(err),        // input wire [31 : 0] B
+  // .SCLR(~i_rst_n),  // input wire SCLR
+  // .P()        // output wire [63 : 0] P
+// );
+multiplier_32	mult_FB_temp (
+	.aclr ( ~i_rst_n ),
+	.clock ( i_clk ),
+	.dataa ( k ),
+	.datab ( err ),
+	.result ( FB_temp )
+	);
+endmodule
