5275b4d65198bf0cef0542ee846cd9623f6e8854
adamShiau <69100634+adamShiau@users.noreply.github.com>
2021-11-25 17:26:44 +0800

    modify fog_top

--- gyro/fog_top_inst.v
+++ gyro/fog_top_inst.v
@@ -1,8 +1,8 @@
 fog_top 
 #(
-TIMER_COUNTER_NUM = 10000,
-DAC_BIT = 16,
-ADC_BIT = 14
+.TIMER_COUNTER_NUM(10000),
+.DAC_BIT(16),
+.ADC_BIT(14)
 )
 u_fog 
 (
@@ -24,8 +24,10 @@ u_fog
 .i_err_offset(), //[31:0]
 .i_avg_sel(), //[2:0]
 .i_err_th(), // [31:0]
+.o_err(), // [31:0]
 /*** feedback_step_gen***/
 .i_gain_sel(), //[3:0]
+.o_step_mon(), // [31:0]
 /*** phase_ramp_gen***/
 .o_ladderWave(), //[DAC_BIT-1:0] 
 .o_phaseRamp() //[DAC_BIT-1:0]
