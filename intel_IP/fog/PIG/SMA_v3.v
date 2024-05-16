c781bc89f041d773ec49fface2ffaa578cd27fa3
adamShiau <ami731025@gmail.com>
2024-05-16 18:37:31 +0800

    d

--- intel_IP/fog/PIG/SMA_v3.v
+++ intel_IP/fog/PIG/SMA_v3.v
@@ -9,11 +9,14 @@ module SMA_v3
 	output signed [31:0] o_data
 	// for simulation
 	, output [31:0] m_count_reg
-	, output [64-1:0] m_sum_reg
+	, output [63:0] m_sum_reg
 	, output [15:0] m_N
 	, output [31:0] m_data_reg
 	, output window_change
 );
+
+`define LOW  1'b0
+`define HIGH 1'b1
 
 //MV select
 localparam SIZE_1 = 		32'd0;
@@ -33,11 +36,21 @@ localparam SIZE_8192 = 		32'd13;
 localparam SIZE_16384 = 	32'd14;
 localparam SIZE_32768 = 	32'd15;
 
+//State machine
+localparam RST = 				4'd0; 
+localparam WAIT = 				4'd1; 
+localparam UPDATE_SUM = 		4'd2;
+localparam SUBSTRACT_OLD = 		4'd3;
+localparam REPLACE_NEW = 		4'd4;
+localparam COUNTET_PROCEED = 	4'd5;
+
+
 // SMA usage
-reg signed [31:0] count_reg, data_reg [0:WINDOW_SIZE-1], r_data_out;
-reg signed [64-1:0] sum_reg;
+reg signed [31:0] count_reg, data_reg [0:WINDOW_SIZE-1];
+reg signed [31:0] sum_reg;
 reg [15:0] N;
 reg [15:0] idx;
+reg [3:0] cstate, nstate;
 //input window usage
 reg [31:0] r_window_sel, r_window_sel_dly;
 // wire window_change;
@@ -45,7 +58,6 @@ reg [31:0] r_window_sel, r_window_sel_dly;
 assign window_change = r_window_sel_dly!=r_window_sel;
 
 assign o_data = (r_window_sel==SIZE_1)? i_data : sum_reg >>> r_window_sel;
-// assign o_data = r_data_out;
 assign m_count_reg = count_reg;
 assign m_sum_reg = sum_reg;
 assign m_N = N;
@@ -80,54 +92,127 @@ always@(posedge i_clk or negedge i_rst_n) begin
 	end
 end
 
-always @(posedge i_clk or negedge i_rst_n) begin
-    if (~i_rst_n) 
-        count_reg <= 0;
-	else begin
-		if(window_change) 
-			count_reg <= 0;
-		
-		if(i_update_strobe) begin
-			if(count_reg > N-1) 
-				count_reg <= 0;
-			else 
-				count_reg <= count_reg + 1;
-		end
-    end
+/*** state register ***/
+always@(posedge i_clk or negedge i_rst_n) begin
+    if(!i_rst_n) cstate <= RST;
+    else cstate <= nstate;
 end
 
-always @(posedge i_clk or negedge i_rst_n) begin
-    if (~i_rst_n) begin
-        sum_reg <= 0;
-		/*** below initialization needs for simulation ***/
-//      for (idx = 0; idx < WINDOW_SIZE; idx = idx + 1) begin
-//          data_reg[idx] <= 0;
-//      end
-    end 
+/*** next state generator ***/
+always@(*) begin
+	if(!i_rst_n) begin
+		nstate = RST;
+	end
+
+	else begin			
+		case(cstate)
+			RST: begin
+				nstate = WAIT;
+			end
+			WAIT: begin
+				if(window_change) nstate = RST;
+				else if(i_update_strobe == `HIGH) nstate = UPDATE_SUM;
+				else nstate = WAIT;
+			end
+			UPDATE_SUM: begin
+				if(window_change) nstate = RST;
+				else nstate = SUBSTRACT_OLD;
+			end
+
+			SUBSTRACT_OLD: begin 
+				if(window_change) nstate = RST;
+				else nstate = REPLACE_NEW;
+			end
+
+			REPLACE_NEW: begin 
+				if(window_change) nstate = RST;
+				else nstate = COUNTET_PROCEED;
+			end
+
+			COUNTET_PROCEED: begin
+				if(window_change) nstate = RST;
+				else nstate = WAIT;
+			end
+			default: nstate = RST;
+		endcase		
+	end
+
+end
+
+// /*** output logic ***/
+always@(posedge i_clk or negedge i_rst_n) begin
+	if(!i_rst_n) begin
+		sum_reg <= 0;
+		count_reg <= 0;
+	end
+	
 	else begin
-		if(window_change) begin
-			sum_reg <= 0;
-//			for (idx = 0; idx < WINDOW_SIZE; idx = idx + 1) begin
-//				data_reg[idx] <= 0;
-//			end
-		end
-		if(i_update_strobe) begin
-			sum_reg <= sum_reg + i_data - data_reg[count_reg];
-			data_reg[count_reg] <= i_data;
-		end
-    end
+		case(cstate)
+			RST: begin
+				sum_reg <= 0;
+       			count_reg <= 0;
+			end
+			WAIT: begin
+
+			end
+			UPDATE_SUM: begin
+				sum_reg <= sum_reg + i_data;
+				
+			end
+			SUBSTRACT_OLD: begin
+				sum_reg <= sum_reg - data_reg[count_reg];
+			end 
+
+			REPLACE_NEW: begin
+				data_reg[count_reg] <= i_data;
+			end
+
+			COUNTET_PROCEED: begin
+				if(count_reg > N-1) begin
+					count_reg <= 0;
+				end
+				else begin
+					count_reg <= count_reg + 1;
+				end
+			end
+		
+			default: begin
+			end
+			
+		endcase
+
+	end
+		
 end
 
 // always @(posedge i_clk or negedge i_rst_n) begin
-//     if (~i_rst_n) begin
-// 		r_data_out <= 0;
-// 	end
+//     if (!i_rst_n) begin
+//         sum_reg <= 0;
+//         count_reg <= 0;
+// 		/*** below initialization needs for simulation ***/
+// //      for (idx = 0; idx < WINDOW_SIZE; idx = idx + 1) begin
+// //          data_reg[idx] <= 0;
+// //      end
+//     end 
 // 	else begin
-// 		if(r_window_sel == SIZE_1)
-// 			r_data_out <= i_data;
-// 		else 
-// 			r_data_out <= sum_reg >>> r_window_sel;
-// 	end
+// 		if(window_change) begin
+// 			count_reg <= 0;
+// 			sum_reg <= 0;
+// //			for (idx = 0; idx < WINDOW_SIZE; idx = idx + 1) begin
+// //				data_reg[idx] <= 0;
+// //			end
+// 		end
+// 		if(i_update_strobe) begin
+// 			if(count_reg > N-1) begin
+// 				count_reg <= 0;
+// 			end
+// 			else begin
+// 				count_reg <= count_reg + 1;
+// 			end
+// 			sum_reg <= sum_reg + i_data - data_reg[count_reg];
+// 			data_reg[count_reg] <= i_data;
+// 		end
+//     end
 // end
 
 endmodule
