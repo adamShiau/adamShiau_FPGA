b90036d2072c618ab9f1bea891ef57eb341d0fe6
adamShiau <ami731025@gmail.com>
2024-11-26 19:07:48 +0800

    Update i2c_controller.v

--- intel_IP/Interface/I2C/i2c_controller.v
+++ intel_IP/Interface/I2C/i2c_controller.v
@@ -9,51 +9,57 @@ module i2c_controller
 	input wire [6:0] 	i_dev_addr,
 	input wire [7:0] 	i_w_data,
 	input wire [7:0] 	i_reg_addr,
-	input wire 			i_enable,
-	input wire 			i_rw_reg,
-	// input wire 			i_drdy,
+	// input wire 			sm_enable,
+	input wire [31:0]	i_ctrl,
+	// input wire 			rw_reg,
+	input wire 			i_drdy,
 
 	output reg [7:0] 	o_rd_data,
-	output wire			o_ready,
-	output wire			o_finish,
+	// output wire			o_ready,
+	// output wire			o_finish,
+	output wire [31:0] 	o_status,
 	output wire 		o_w_enable,
-	output wire [7:0] 	sm,
+	// output wire [7:0] 	sm,
 	output wire			i2c_clk_out,
 	output wire			i2c_scl,
 	inout 				i2c_sda
 	);
 
-	localparam IDLE = 0;
-	localparam START = 1;
-	localparam ADDRESS = 2;
-	localparam READ_ACK = 3;
-	localparam READ_ACK_B = 4;
-	localparam REG_ADDR  = 5;
-	localparam READ_ACK2 = 6;
-	localparam READ_ACK2_B = 7;
-	localparam READ_DATA = 8;
-	localparam WRITE_ACK = 9;
-	localparam READ_DATA2 = 15;
-	localparam WRITE_ACK2 = 16;
-	localparam READ_DATA3 = 17;
-	localparam WRITE_ACK3 = 18;
-	localparam READ_DATA4 = 19;
-	localparam WRITE_ACK4 = 20;
-	localparam READ_DATA5 = 21;
-	localparam WRITE_ACK5 = 22;
-	localparam READ_DATA6 = 23;
-	localparam WRITE_ACK6 = 24;
-	localparam READ_DATA7 = 25;
-	localparam WRITE_ACK7 = 26;
-	localparam READ_DATA8 = 27;
-	localparam WRITE_ACK8 = 28;
-	localparam READ_DATA9 = 29;
-	localparam WRITE_ACK9 = 31;
-	localparam WRITE_DATA = 10;
-	localparam READ_ACK3 = 11;
-	localparam READ_ACK3_B = 12;	
-	localparam STOP = 13;
-	localparam STOP2 = 14;
+	localparam IDLE 		= 	0;
+	localparam START 		= 	1;
+	localparam ADDRESS 		= 	2;
+	localparam READ_ACK 	= 	3;
+	localparam READ_ACK_B 	= 	4;
+	localparam REG_ADDR  	= 	5;
+	localparam READ_ACK2 	= 	6;
+	localparam READ_ACK2_B 	= 	7;
+	localparam WRITE_DATA 	= 	8;
+	localparam READ_ACK3 	= 	9;
+	localparam READ_ACK3_B 	= 	10;	
+	localparam READ_DATA 	= 	11;
+	localparam WRITE_ACK 	= 	12;
+	localparam READ_DATA2 	= 	13;
+	localparam WRITE_ACK2 	= 	14;
+	localparam READ_DATA3 	= 	15;
+	localparam WRITE_ACK3 	= 	16;
+	localparam READ_DATA4 	= 	17;
+	localparam WRITE_ACK4 	= 	18;
+	localparam READ_DATA5 	= 	19;
+	localparam WRITE_ACK5 	= 	20;
+	localparam READ_DATA6 	= 	21;
+	localparam WRITE_ACK6 	= 	22;
+	localparam READ_DATA7 	= 	23;
+	localparam WRITE_ACK7 	= 	24;
+	localparam READ_DATA8 	= 	25;
+	localparam WRITE_ACK8 	= 	26;
+	localparam READ_DATA9 	= 	27;
+	localparam WRITE_ACK9 	= 	28;
+	localparam READ_DATA10 	= 	29;
+	localparam WRITE_ACK10 	= 	30;
+	localparam READ_DATA11 	= 	31;
+	localparam WRITE_ACK11 	= 	32;
+	localparam STOP 		= 	33;
+	localparam STOP2 		= 	34;
 
 	reg [7:0] state;
 	reg [7:0] saved_addr;
@@ -70,11 +76,21 @@ module i2c_controller
 	reg rw = 0;
 	reg r_drdy = 0;
 
+	wire sm_enable, rw_reg;
+	wire [1:0] op_mode;
+	// control register
+	assign sm_enable = i_ctrl[0];
+	assign rw_reg = i_ctrl[1];
+	assign op_mode = i_ctrl[3:2]; //00: CPU 1 byte, 01: CPU 11 bytes, 10: FPGA 11 bytes, 11: reserved
+	// status register
+	assign o_status[0] = ((i_rst_n == 1) && (state == IDLE)) ? 1 : 0; //ready
+
+
 	assign i2c_clk_out = i2c_clk;
 	assign sm = state; 
 	assign o_finish = ( (write_done == 0) && (state == STOP2) )? 1:0;
 	assign o_w_enable = write_enable; 
-	assign o_ready = ((i_rst_n == 1) && (state == IDLE)) ? 1 : 0;
+	
 	// assign i2c_scl = (i2c_scl_enable == 0 ) ? 1 : i2c_clk;
 	// assign i2c_scl = (i2c_scl_enable2 == 0 ) ? 0 : ( (i2c_scl_enable == 0 ) ? 1 : i2c_clk) ;
 	assign i2c_scl = (i2c_scl_enable == 0 ) ? 0 : ( (i2c_scl_enable == 1 ) ? 1 : i2c_clk) ;
@@ -112,14 +128,14 @@ module i2c_controller
 		else begin
 			case(state)
 				IDLE: begin
-					// if (i_enable && r_drdy) begin
-					if (i_enable) begin
+					// if (sm_enable && r_drdy) begin
+					if (sm_enable) begin
 						state <= START;
-						if(i_rw_reg == 1'b1) begin// read reg mode
+						if(rw_reg == 1'b1) begin// read reg mode
 							if(write_done == 1'b0) rw <= 1'b0;
 							else rw <= 1'b1;
 						end
-						else rw <= 1'b0;
+						else rw <= 1'b0; // write reg mode
 					end
 					else state <= IDLE;
 
@@ -158,7 +174,7 @@ module i2c_controller
 				end
 
 				REG_ADDR: begin 
-					if(i_rw_reg == 1'b1) //read reg mode
+					if(rw_reg == 1'b1) //read reg mode
 						write_done <= 1'b1;
 
 					if(counter == 0) begin
@@ -171,9 +187,9 @@ module i2c_controller
 				end
 
 				READ_ACK2_B: begin 
-					counter <= 7;
-					if( i_rw_reg == 1'b1) state <= STOP;
+					if( rw_reg == 1'b1) state <= STOP;
 					else begin
+						counter <= 7;
 						saved_data = i_w_data;
 						state <= WRITE_DATA;
 					end
@@ -195,16 +211,19 @@ module i2c_controller
 					end else counter <= counter - 1;
 				end
 
-				READ_DATA: begin
+				READ_DATA: begin//11
 					if(write_done == 1'b1) write_done = 1'b0;
 					o_rd_data[counter] <= i2c_sda;
 					if (counter == 0) state <= WRITE_ACK;
 					else counter <= counter - 1;
 				end
 				
-				WRITE_ACK: begin
-					counter <= 7;
-					state <= READ_DATA2;
+				WRITE_ACK: begin//12
+					if(~op_mode) state <= STOP; //00, CPU read 1 byte
+					else begin// else, CPU/FPGA read 11 bytes
+						counter <= 7;
+						state <= READ_DATA2;
+					end
 				end
 
 				READ_DATA2: begin
@@ -284,6 +303,27 @@ module i2c_controller
 				end
 				WRITE_ACK9: begin
 					counter <= 7;
+					state <= READ_DATA10;
+				end
+
+				READ_DATA10: begin
+					o_rd_data[counter] <= i2c_sda;
+					if (counter == 0) state <= WRITE_ACK10;
+					else counter <= counter - 1;
+				end
+
+				WRITE_ACK10: begin
+					counter <= 7;
+					state <= READ_DATA11;
+				end
+
+				READ_DATA11: begin
+					o_rd_data[counter] <= i2c_sda;
+					if (counter == 0) state <= WRITE_ACK11;
+					else counter <= counter - 1;
+				end
+
+				WRITE_ACK11: begin
 					state <= STOP;
 				end
 
