5f9524c8d6c70f163a6cb0ffd07b066054dcdfb5
adamShiau <ami731025@gmail.com>
2025-04-04 23:10:03 +0800

    Update output_fn.c

--- intel_IP/IRIS/NIOS/output_fn.c
+++ intel_IP/IRIS/NIOS/output_fn.c
@@ -26,19 +26,88 @@ void acq_rst (cmd_ctrl_t* rx, my_sensor_t data, fog_parameter_t fog_parameter)
 }
 
 // void acq_fog (cmd_ctrl_t* rx, my_sensor_t data, alt_u8* sync, fog_parameter_t fog_parameter)
+// void acq_fog (cmd_ctrl_t* rx, my_sensor_t data, fog_parameter_t fog_parameter)
+// {
+//     my_float_t time, err1, step1, err2, step2, err3, step3;
+//     my_float_t temp3, temp2, temp1;
+//     my_float_t ax, ay, az, acc_temp;
+    
+//     /*** update container parameters */
+//     float sf_1_slope, sf_2_slope, sf_3_slope;
+//     float sf_1_offset, sf_2_offset, sf_3_offset;
+
+//     if(rx->select_fn == SEL_FOG) {
+//         rx->select_fn = SEL_IDLE; //clear select_fn
+//         DEBUG_PRINT("select acq_fog mode\n");
+//         if(rx->value == INT_SYNC) { //internal sync mode
+//             DEBUG_PRINT("acq_fog select internal mode\n");
+//             rx->run = 1;
+//         }
+//         else if(rx->value == EXT_SYNC) { //external sync mode
+//             DEBUG_PRINT("acq_fog select external mode\n");
+//             rx->run = 1;
+//         }
+//         else if(rx->value == STOP_RUN) { //stop
+//             DEBUG_PRINT("acq_fog select stop\n");
+//             rx->run = 0;
+//         }
+//     }
+//     if(rx->run == 1) {
+
+//             sf_1_slope = fog_parameter.paramZ[17].data.float_val;
+//             sf_1_offset = fog_parameter.paramZ[18].data.float_val;
+//             sf_2_slope = fog_parameter.paramZ[19].data.float_val;
+//             sf_2_offset = fog_parameter.paramZ[20].data.float_val;
+//             sf_3_slope = fog_parameter.paramZ[21].data.float_val;
+//             sf_3_offset = fog_parameter.paramZ[22].data.float_val;
+
+//             time.float_val = data.time.time.float_val;
+//             temp1.float_val = data.temp.tempx.float_val*ADC_CONV_TEMP-273.15;
+//             temp2.float_val = data.temp.tempy.float_val*ADC_CONV_TEMP-273.15;
+//             temp3.float_val = data.temp.tempz.float_val*ADC_CONV_TEMP-273.15;
+//             err3.int_val = data.fog.fogz.err.int_val;
+//             step3.float_val = data.fog.fogz.step.float_val*(sf_3_slope*temp1.float_val + sf_3_offset); 
+//             // step3.float_val = data.fog.fogz.step.float_val*(sf_1_slope*temp1.float_val + sf_1_offset); // temporary use temp1 for IRIS configuration
+
+//             ax.float_val = data.adxl357.ax.float_val*SENS_ADXL357_20G;
+//             ay.float_val = data.adxl357.ay.float_val*SENS_ADXL357_20G;
+//             az.float_val = data.adxl357.az.float_val*SENS_ADXL357_20G;
+//             acc_temp.float_val = 233.2873 - 0.1105*data.adxl357.temp.float_val;
+            
+//             alt_u8* imu_data = (alt_u8*)malloc(20); // KVH_HEADER:4 + time:4 + err:4 + fog:4 + temp:4
+// 			alt_u8 CRC32[4];
+
+//             memcpy(imu_data, KVH_HEADER, 4);
+//             memcpy(imu_data+4, time.bin_val, 4); 
+//             memcpy(imu_data+8, err3.bin_val, 4); 
+//             memcpy(imu_data+12, step3.bin_val, 4); 
+//             memcpy(imu_data+16, temp1.bin_val, 4); // AIN0
+//             crc_32(imu_data, 20, CRC32);
+//             free(imu_data);
+
+//             SerialWrite((alt_u8*)KVH_HEADER, 4); 
+//             SerialWrite(time.bin_val, 4); 
+//             SerialWrite(err3.bin_val, 4); 
+//             SerialWrite(step3.bin_val, 4); 
+//             SerialWrite(temp1.bin_val, 4); 
+//             SerialWrite(CRC32, 4); 
+
+//             // INFO_PRINT("%f, %f, %f. %f\n", ax.float_val, ay.float_val, az.float_val, acc_temp.float_val);
+//             // INFO_PRINT("%f, %f, %f\n", temp1.float_val, temp2.float_val, temp3.float_val);
+//     }
+// }
+
 void acq_fog (cmd_ctrl_t* rx, my_sensor_t data, fog_parameter_t fog_parameter)
 {
-    my_float_t time, err1, step1, err2, step2, err3, step3;
-    my_float_t temp3, temp2, temp1;
-    my_float_t ax, ay, az, acc_temp;
-    
-    /*** update container parameters */
-    float sf_1_slope, sf_2_slope, sf_3_slope;
-    float sf_1_offset, sf_2_offset, sf_3_offset;
+    my_float_t err, step_cali, temp;
+        
+    float step_comp, sf_comp_gyro, bias_comp_gyro;
+    static alt_u8 ch = 0;
 
     if(rx->select_fn == SEL_FOG) {
         rx->select_fn = SEL_IDLE; //clear select_fn
-        DEBUG_PRINT("select acq_fog mode\n");
+        ch = rx->ch;
+        DEBUG_PRINT("select acq_fog mode with ch%u\n", ch);
         if(rx->value == INT_SYNC) { //internal sync mode
             DEBUG_PRINT("acq_fog select internal mode\n");
             rx->run = 1;
@@ -53,49 +122,59 @@ void acq_fog (cmd_ctrl_t* rx, my_sensor_t data, fog_parameter_t fog_parameter)
         }
     }
     if(rx->run == 1) {
-
-            sf_1_slope = fog_parameter.paramZ[17].data.float_val;
-            sf_1_offset = fog_parameter.paramZ[18].data.float_val;
-            sf_2_slope = fog_parameter.paramZ[19].data.float_val;
-            sf_2_offset = fog_parameter.paramZ[20].data.float_val;
-            sf_3_slope = fog_parameter.paramZ[21].data.float_val;
-            sf_3_offset = fog_parameter.paramZ[22].data.float_val;
-
-            time.float_val = data.time.time.float_val;
-            temp1.float_val = data.temp.tempx.float_val*ADC_CONV_TEMP-273.15;
-            temp2.float_val = data.temp.tempy.float_val*ADC_CONV_TEMP-273.15;
-            temp3.float_val = data.temp.tempz.float_val*ADC_CONV_TEMP-273.15;
-            err3.int_val = data.fog.fogz.err.int_val;
-            step3.float_val = data.fog.fogz.step.float_val*(sf_3_slope*temp1.float_val + sf_3_offset); 
-            // step3.float_val = data.fog.fogz.step.float_val*(sf_1_slope*temp1.float_val + sf_1_offset); // temporary use temp1 for IRIS configuration
-
-            ax.float_val = data.adxl357.ax.float_val*SENS_ADXL357_20G;
-            ay.float_val = data.adxl357.ay.float_val*SENS_ADXL357_20G;
-            az.float_val = data.adxl357.az.float_val*SENS_ADXL357_20G;
-            acc_temp.float_val = 233.2873 - 0.1105*data.adxl357.temp.float_val;
-            
+        switch(ch) {
+            case X_AXIS:
+                err.float_val = data.fog.fogx.err.float_val;
+                temp.float_val = data.temp.tempx.float_val;
+                sf_comp_gyro = SF_temp_compensation_1st_order_fog(data, fog_parameter, X_AXIS);
+                bias_comp_gyro = BIAS_temp_compensation_1st_order_fog_3T(data, fog_parameter, X_AXIS);
+                step_comp = data.fog.fogx.step.float_val * sf_comp_gyro - bias_comp_gyro;
+                /*** calculate mis-alignment calibrated gyro data */
+                step_cali.float_val = misalignment_calibration(step_comp, 0, 0, fog_parameter, MIS_CALI_GYRO).x.float_val;
+            break;
+            case Y_AXIS:
+                err.float_val = data.fog.fogy.err.float_val;
+                temp.float_val = data.temp.tempy.float_val;
+                sf_comp_gyro = SF_temp_compensation_1st_order_fog(data, fog_parameter, Y_AXIS);
+                bias_comp_gyro = BIAS_temp_compensation_1st_order_fog_3T(data, fog_parameter, Y_AXIS);
+                step_comp = data.fog.fogx.step.float_val * sf_comp_gyro - bias_comp_gyro;
+                /*** calculate mis-alignment calibrated gyro data */
+                step_cali.float_val = misalignment_calibration(0, step_comp, 0, fog_parameter, MIS_CALI_GYRO).y.float_val;
+            break;
+            case Z_AXIS:
+                err.float_val = data.fog.fogz.err.float_val;
+                temp.float_val = data.temp.tempz.float_val;    
+                sf_comp_gyro = SF_temp_compensation_1st_order_fog(data, fog_parameter, Z_AXIS);
+                bias_comp_gyro = BIAS_temp_compensation_1st_order_fog_3T(data, fog_parameter, Z_AXIS);
+                step_comp = data.fog.fogx.step.float_val * sf_comp_gyro - bias_comp_gyro;
+                /*** calculate mis-alignment calibrated gyro data */
+                step_cali.float_val = misalignment_calibration(0, 0, step_comp, fog_parameter, MIS_CALI_GYRO).z.float_val;
+            break;
+        }
+        
             alt_u8* imu_data = (alt_u8*)malloc(20); // KVH_HEADER:4 + time:4 + err:4 + fog:4 + temp:4
 			alt_u8 CRC32[4];
 
             memcpy(imu_data, KVH_HEADER, 4);
-            memcpy(imu_data+4, time.bin_val, 4); 
-            memcpy(imu_data+8, err3.bin_val, 4); 
-            memcpy(imu_data+12, step3.bin_val, 4); 
-            memcpy(imu_data+16, temp1.bin_val, 4); // AIN0
+            memcpy(imu_data+4, data.time.time.bin_val, 4); 
+            memcpy(imu_data+8, err.bin_val, 4); 
+            memcpy(imu_data+12, step_cali.bin_val, 4); 
+            memcpy(imu_data+16, temp.bin_val, 4); // AIN0
             crc_32(imu_data, 20, CRC32);
             free(imu_data);
 
             SerialWrite((alt_u8*)KVH_HEADER, 4); 
-            SerialWrite(time.bin_val, 4); 
-            SerialWrite(err3.bin_val, 4); 
-            SerialWrite(step3.bin_val, 4); 
-            SerialWrite(temp1.bin_val, 4); 
+            SerialWrite(data.time.time.bin_val, 4); 
+            SerialWrite(err.bin_val, 4); 
+            SerialWrite(step_cali.bin_val, 4); 
+            SerialWrite(temp.bin_val, 4); 
             SerialWrite(CRC32, 4); 
 
             // INFO_PRINT("%f, %f, %f. %f\n", ax.float_val, ay.float_val, az.float_val, acc_temp.float_val);
             // INFO_PRINT("%f, %f, %f\n", temp1.float_val, temp2.float_val, temp3.float_val);
     }
 }
+
 
 void acq_imu (cmd_ctrl_t* rx, my_sensor_t data, fog_parameter_t fog_parameter)
 {
