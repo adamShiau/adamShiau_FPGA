faae2ed0153121f761d390a114733107af6896ac
adamShiau <69100634+adamShiau@users.noreply.github.com>
2025-12-02 19:41:38 +0800

    f

---
+++ intel_IP/HINS/NIOS/HINS_CPU_V1_0/HINS_CPU_V1_0.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>  // malloc(), free()
+#include <string.h>  // memcpy()
+#include "sys/alt_irq.h"
+#include "system.h"
+
+int main(void)
+{
+    printf("Hello World\n");
+    return 0;
+}
---
+++ intel_IP/HINS/PRJ/HINS_PRJ_V1/software/HINS_CPU_V1_0/obj/default/D/github/adamShiau_FPGA/intel_IP/HINS/NIOS/HINS_CPU_V1_0/HINS_CPU_V1_0.d
@@ -0,0 +1,16 @@
+obj/default//D/github/adamShiau_FPGA/intel_IP/HINS/NIOS/HINS_CPU_V1_0/HINS_CPU_V1_0.o: \
+ D:/github/adamShiau_FPGA/intel_IP/HINS/NIOS/HINS_CPU_V1_0/HINS_CPU_V1_0.c \
+ ../HINS_CPU_V1_0_bsp//HAL/inc/sys/alt_irq.h \
+ ../HINS_CPU_V1_0_bsp//HAL/inc/nios2.h \
+ ../HINS_CPU_V1_0_bsp//HAL/inc/alt_types.h ../HINS_CPU_V1_0_bsp/system.h \
+ ../HINS_CPU_V1_0_bsp/linker.h
+
+../HINS_CPU_V1_0_bsp//HAL/inc/sys/alt_irq.h:
+
+../HINS_CPU_V1_0_bsp//HAL/inc/nios2.h:
+
+../HINS_CPU_V1_0_bsp//HAL/inc/alt_types.h:
+
+../HINS_CPU_V1_0_bsp/system.h:
+
+../HINS_CPU_V1_0_bsp/linker.h:
