b73d0a001b8088b900ab4885440769a19d3a06d9
adamShiau <ami731025@gmail.com>
2025-12-12 00:24:30 +0800

    d

---
+++ intel_IP/HINS/PRJ/HINS_PRJ_V1/software/TEST/obj/default/hello_world.d
@@ -0,0 +1,16 @@
+obj/default/hello_world.o: hello_world.c ../TEST_bsp//HAL/inc/alt_types.h \
+ ../TEST_bsp/system.h ../TEST_bsp/linker.h \
+ ../TEST_bsp//drivers/inc/altera_avalon_uart_regs.h \
+ ../TEST_bsp//HAL/inc/io.h ../TEST_bsp//HAL/inc/alt_types.h
+
+../TEST_bsp//HAL/inc/alt_types.h:
+
+../TEST_bsp/system.h:
+
+../TEST_bsp/linker.h:
+
+../TEST_bsp//drivers/inc/altera_avalon_uart_regs.h:
+
+../TEST_bsp//HAL/inc/io.h:
+
+../TEST_bsp//HAL/inc/alt_types.h:
