357da8ac0d9cd5d1a634900e233f67e96a7aec24
adamShiau <69100634+adamShiau@users.noreply.github.com>
2026-03-16 17:21:11 +0800

    d

---
+++ intel_IP/HINS/NIOS/HINS_CPU_V1_4/include/acq_rst.h
@@ -0,0 +1,14 @@
+#ifndef __ACQ_RST_H
+#define __ACQ_RST_H
+
+
+#include "stdio.h"
+#include <stdlib.h>  // malloc(), free()
+#include <string.h>  // memcpy()
+#include "common.h"
+
+// extern alt_u32 g_time[12];
+
+void acq_rst (cmd_ctrl_t*, my_sensor_t, fog_parameter_t);
+
+#endif /* __ACQ_RST_H */
---
+++ intel_IP/HINS/NIOS/HINS_CPU_V1_4/include/nios2_var_addr.h
@@ -0,0 +1,55 @@
+/*** This file defines the Address of NIOSii Variabe IP used in HINS***/
+
+#define	var_freq_cnt		            0
+#define var_amp_H		                1
+#define var_amp_L		                2
+#define var_polarity		            3
+#define var_wait_cnt		            4
+#define var_avg_sel		                5
+#define var_gainSel_step		        6
+#define var_const_step		            7 
+#define var_fb_ON		                8
+#define var_gainSel_ramp    	        9
+#define var_err_offset                  10
+/*** 11 ~ 32 reserve for another two channels */
+#define var_sync_count		            33
+#define var_timer_rst                   34
+#define var_i2c_ads122c04_dev_addr      35
+#define var_i2c_ads122c04_reg_addr      36
+#define var_i2c_ads122c04_w_data        37
+#define var_i2c_ads122c04_ctrl          38
+#define var_i2c_EEPROM_dev_addr         39
+#define var_i2c_EEPROM_reg_addr         40
+#define var_i2c_EEPROM_w_data           41
+#define var_i2c_EEPROM_ctrl		        42
+#define var_i2c_IMU_dev_addr	        43
+#define var_i2c_IMU_reg_addr		    44
+#define var_i2c_IMU_w_data              45
+#define var_i2c_IMU_ctrl                46
+
+
+#define i_var_i2c_ads122c04_status	    60 + 0
+#define i_var_i2c_ads122c04_rdata_1	    60 + 1
+#define i_var_i2c_ads122c04_rdata_2	    60 + 2
+#define i_var_i2c_ads122c04_rdata_3	    60 + 3
+#define i_var_i2c_ads122c04_rdata_4	    60 + 4
+#define i_var_i2c_EEPROM_status	        60 + 5
+#define i_var_i2c_EEPROM_rdata_1	    60 + 6
+#define i_var_i2c_EEPROM_rdata_2	    60 + 7
+#define i_var_i2c_EEPROM_rdata_3	    60 + 8
+#define i_var_i2c_EEPROM_rdata_4	    60 + 9
+#define i_var_i2c_IMU_status	        60 + 10
+#define i_var_i2c_IMU_rdata_1	        60 + 11
+#define i_var_i2c_IMU_rdata_2	        60 + 12
+#define i_var_i2c_IMU_rdata_3           60 + 13
+#define i_var_i2c_IMU_rdata_4           60 + 14
+#define i_var_i2c_IMU_rdata_5           60 + 15
+#define i_var_i2c_IMU_rdata_6           60 + 16
+#define i_var_i2c_IMU_rdata_7           60 + 17
+#define i_var_err                       60 + 18
+#define i_var_step                      60 + 19
+#define i_var_timer                     60 + 20
+#define i_var_reg_1_err_signal_gen      60 + 21
+#define i_var_reg_1_Feedback_control    60 + 22
+
+
